0441f54647d6a5af835c2568de059011d577e970
SH20RAJ <shaswatraj3@gmail.com>
2026-06-04 00:19:47 +0530

    Rename project references from "unstory" to "campusloop" in configuration files

--- cloudflare-env.d.ts
+++ cloudflare-env.d.ts
@@ -5,7 +5,7 @@ interface __BaseEnv_CloudflareEnv {
 	IMAGES: ImagesBinding;
 	ASSETS: Fetcher;
 	NEXTJS_ENV: string;
-	WORKER_SELF_REFERENCE: Fetcher /* unstory */;
+	WORKER_SELF_REFERENCE: Fetcher /* campusloop */;
 }
 declare namespace Cloudflare {
 	interface Env extends __BaseEnv_CloudflareEnv {}
